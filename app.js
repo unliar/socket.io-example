2d0b3d68e7bd7da123b801aa5d444596d12fea7c
blessyou <370732889@qq.com>
2017-04-05 10:56:33 +0800

    update readme

--- app.js
+++ app.js
@@ -21,29 +21,29 @@ server.on('listening', () => console.log('listening in ', port));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 app.use(cookieParser());
-app.use('/demo', express.static(path.join(__dirname, 'public/vue/mydemo')));
+app.use(express.static(path.join(__dirname, 'public/vue/mydemo')));
 
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.send('error');
 });
 
 module.exports = app
