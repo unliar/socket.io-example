69934dca2cf42192857475ce04bc4e5f3beb6991
blessyou <370732889@qq.com>
2017-05-29 17:09:50 +0800

    验证相关API

--- socketMethods/top.js
+++ socketMethods/top.js
@@ -1,21 +1,37 @@
 var io = require('socket.io')();
-
+var cookieParser = require('cookie-parser');
+//连接事件
 io.on('connection', (socket) => {
-    console.log('socketio已经连接');
-
+    //console.log('socket服务信息', socket);
+    //console.log('socket服务信息client', socket.client);
+    //console.log('socket服务信息conn', socket.conn);
+    console.log('socket请求信息', socket.request.headers.cookie)
+    console.log('socket服务信息id', socket.id);
+    console.log('socket服务信息rooms', socket.rooms);
+    io.clients((err, users) => console.log('当前用户数', users))
+    //console.log('namespace信息', io.nsps);
+    //发送群消息
+    io.emit('one', '来自命名空间的群发')
     //demo-start
     socket.on('one', res => {
         //receive data from browser emit
         //打印出来自浏览器的emit数据
-        console.log(res);
+        console.log('浏览器emit的数据', res);
         //response some data to browser;
         //向浏览器发送数据
-        socket.emit('one', { status: 'i have got your message from browser，service starts', data: res });
+        socket.emit('one', {
+            status: 'i have got your message from browser，service starts',
+            data: res
+        });
+        //获取当前所有客户端ID
+        io.clients((err, clients) => {
+            console.log('客户端ID列表', clients)
+        })
+
+
     });
-
-
 })
 
 exports.listen = (server) => {
     return io.listen(server)
-} 
+}
